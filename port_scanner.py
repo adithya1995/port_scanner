fdc305dd53e94e3d5f84ffcf964609834f2015e5
Adithya Karthikeyan <32382556+adithya1995@users.noreply.github.com>
2018-02-23 12:33:20 -0800

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,21 @@
+import socket
+
+hosts = ["192.168.1.1", "192.168.2.1", "192.168.2.2", "192.168.2.10"]
+
+
+
+#ports = range(lowPort, highPort)
+
+for host in hosts:
+    for port in range(1,1025):
+        try:
+            print "Connecting to" + host + ":" + str(port)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(5)
+            result = s.connect_ex((host, port))
+            #banner = s.recv(1024) # Will cause a timeout w/o a response
+            if result == 0:
+                print "  [*] Port " + str(port) + " open!"
+            s.close()
+        except:
+            pass
